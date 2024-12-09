6f74feed4e028a0b32d6621da2cf20a2e70c52e2
Laus-Fdz <luryfernandez98@alumnos.cei.es>
2024-12-09 16:01:18 +0100

    cambios

--- javascript.js
+++ javascript.js
@@ -1,56 +1,149 @@
+// -----------------------------------
+//1. Nuestros datos
+//-----------------------------------
+
 // Lista de canciones y artistas
-const listaCanciones = ["Roslyn", "Old Pine", "For Emma", "Oats in the Water", "Beach Baby"];
-const listaArtistas = ["Bon Iver", "Ben Howard", "Bon Iver", "Ben Howard", "Bon Iver"];
+// const listaCanciones = ["Roslyn", "Old Pine", "For Emma", "Oats in the Water", "Beach Baby"];
+// const listaArtistas = ["Bon Iver", "Ben Howard", "Bon Iver", "Ben Howard", "Bon Iver"];
 
-// Selección del contenedor
-const divListaCanciones = document.getElementById("listaCanciones"); // Se quita el #, ya que getElementById no lo requiere
+// const listaMp3 = ["./audio/Bon Iver & St. Vincent - Roslyn (Lyrics).mp3"]
 
-// Ejercicio 3: Mostrar lista de canciones y artistas
-listaCanciones.forEach((cancion, idx) => {
-    console.log(`${cancion} - id: ${idx}`);
+const listaCanciones = [
+    {
+        artista: "Bon Iver",
+        url: "./audio/Bon Iver & St. Vincent - Roslyn (Lyrics).mp3",
+        titulo: "Roslyn"
 
-    const song = listaCanciones[idx];
-    const artist = listaArtistas[idx];
+    },    {
+        artista: "Ben Howard",
+        url: "./",
+        titulo: "Old Pine"
 
-    // innerHTML para agregar canciones y artistas con un atributo id único
-    divListaCanciones.innerHTML += `<div id="cancion -${idx}" class="Lista-cancion"> 
-            ${song} <br> ${artist} <br>
-        </div>`;
-});
+    },    {
+        artista: "Bon Iver",
+        url: "./",
+        titulo: "For Emma"
 
-// Ejercicio 4: Botón para reproducir canción 3
-const btnTema3 = document.querySelector("#btnTema3");
-btnTema3.addEventListener("click", () => {
-    // índice 2 es la canción número 3 porque los arrays comienzan en 0
-    console.log("Cancion: ", listaCanciones[2]); 
-    console.log("Artista: ", listaArtistas[2]);
-});
+    },    {
+        artista: "Ben Howard",
+        url: "./",
+        titulo: "Oats in the Water"
 
-// Ejercicio 5: Botones de control
+    }
+    ,    {
+        artista: "Bon Iver",
+        url: "./",
+        titulo: "Beach Bay"
+
+    }
+];
+
+//-----------------------------------
+// 2. Constantes, Variables y QuerySelectors
+//-----------------------------------
+
 // Seleccionamos los botones del DOM
 const btnAnt = document.querySelector("#btnAnt");
 const btnPlay = document.querySelector("#btnPlay");
 const btnPausa = document.querySelector("#btnPausa");
 const btnSig = document.querySelector("#btnSig");
 
-// Agregamos eventos a cada botón
-btnAnt.addEventListener('click', (event) => {
-    console.log(event.target.id);
-});
-btnPlay.addEventListener('click', (event) => {
-    console.log(event.target.id);
-});
-btnPausa.addEventListener('click', (event) => {
-    console.log(event.target.id);
-});
-btnSig.addEventListener('click', (event) => {
-    console.log(event.target.id);
-});
+// Se quita el #, ya que getElementById no lo requiere
+const divListaCanciones = document.getElementById("listaCanciones"); 
 
-// Ejercicio 6: Mostrar nombre de canción y artista al hacer clic en cada div
+// Botón para reproducir canción 3
+const btnTema3 = document.querySelector("#btnTema3");
+
 // Seleccionamos todos los divs generados con la clase "Lista-cancion"
 const divsCanciones = document.querySelectorAll(".Lista-cancion");
 const divsPlayingSong = document.getElementById("playingSong");
+
+// obtener nuestro audio
+const audio = document.getElementById("reproductorAudio");
+
+
+
+//-----------------------------------
+// 3. Funciones y even listeners
+//-----------------------------------
+
+// Agregamos eventos a cada botón
+btnAnt.addEventListener('click', (event) => {
+    imprimirReproduciendo();
+});
+btnPlay.addEventListener('click', (event) => {
+    imprimirReproduciendo();
+});
+btnPausa.addEventListener('click', (event) => {
+    imprimirReproduciendo();
+});
+btnSig.addEventListener('click', (event) => {
+    imprimirReproduciendo();
+});
+
+function imprimirReproduciendo(i) {
+    const song = listaCanciones[i].titulo;
+    const artist = listaArtistas[i].artista;
+    const url = listaCanciones[i].url;
+    
+    console.log("Artista: " + artist + ", Canción: " + song);
+
+    divsPlayingSong.innerHTML = `<div> 
+    Canción: ${song}<br> Artista: ${artist}
+    </div>`;
+    // cambiar el src de nuestro audio HTML
+    console.log(audio);
+    audio.src = listaMp3[idCancionActual];
+}
+
+
+// Bonotes de siguiente y anterior
+
+let idCancionActual = 0; // primera canción
+btnSig.addEventListener("click", () => {
+    if (idCancionActual == listaCanciones.length - 1) {
+        idCancionActual = 0;
+    } else {
+        idCancionActual++;
+    }
+
+    imprimirReproduciendo(idCancionActual);
+});
+btnAnt.addEventListener("click", () => {
+    if (idCancionActual <= 0) {
+        idCancionActual = listaCanciones.length - 1;
+    } else {
+        idCancionActual--;
+    }
+
+    imprimirReproduciendo(idCancionActual);
+});
+
+btnPlay.addEventListener("click", () => {
+    audio.play();
+});
+
+btnPausa.addEventListener("click", () => {
+    audio.pause();
+});
+
+
+//-----------------------------------
+// 4. Su código una vez cargado todo lo demás
+//-----------------------------------
+
+// Mostrar lista de canciones y artistas
+listaCanciones.forEach((cancion, idx) => {
+    console.log(`${cancion} - id: ${idx}`);
+
+    const song = listaCanciones[idx].titulo;
+    const artist = listaArtistas[idx].artista;
+
+    // innerHTML para agregar canciones y artistas con un atributo id único
+    divListaCanciones.innerHTML += `<div id="cancion -${idx}" class="Lista-cancion"> 
+            ${song} <br> ${artist} <br>
+        </div>`;
+});
 
 divsCanciones.forEach((divCancion, i) => {
     divCancion.addEventListener("click", () => {
@@ -64,26 +157,5 @@ divsCanciones.forEach((divCancion, i) => {
     });
 });
 
-function imprimirReproduciendo(i){
-    const song = listaCanciones[i];
-    const artist = listaArtistas[i];
-    console.log("Artista: " + artist + ", Canción: " + song);
 
-    divsPlayingSong.innerHTML = `<div> 
-    Canción: ${song}<br> Artista: ${artist}
-    </div>`;
-}
-
-
-// Bonotes de siguiente y anterior
-
-let idCancionActual = 0; // primera canción
-btnSig. addEventListener("click", ()=>{
-    idCancionActual++;
-    imprimirReproduciendo(idCancionActual);
-});
-btnAnt. addEventListener("click", ()=>{
-    idCancionActual--;
-    imprimirReproduciendo(idCancionActual);
-});
 
